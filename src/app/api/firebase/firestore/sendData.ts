3ac6baa14388bda699d39f3c9e6859f0a670ed59
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-07-14 19:16:28 -0400

    another

--- src/app/api/firebase/firestore/sendData.ts
+++ src/app/api/firebase/firestore/sendData.ts
@@ -1,11 +1,21 @@
 import { doc, getFirestore, setDoc } from "firebase/firestore"; 
-import firebase_app from "../firebase/config";
+import firebase_app from "../config";
 
 export const db = getFirestore(firebase_app);
-export default async function handler(data: string) {
+export default async function sendData(data: string) {
+  let result = null;
+  let error = null;
+
+  try {
+
   await setDoc(doc(db, "cities", "LA"), {
     name: "Los Angeles",
     state: "CA",
     country: "USA"
   });
+  } catch (e) {
+    error = e;
+  }
+
+  return { result, error };
 }
