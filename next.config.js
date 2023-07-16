e4d2669aac17b7fcc51bb40bf48a4e81f4dd95c9
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-07-15 21:06:50 -0400

    really?

--- next.config.js
+++ next.config.js
@@ -1,26 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    async headers() {
-        return [
-          {
-            source: "/api/firebase/firestore/sendData", // Replace "/api/*" with your Firestore API endpoint
-            headers: [
-              {
-                key: "Access-Control-Allow-Origin",
-                value: "*", // Replace "*" with your desired origins or restrict it to specific domains
-              },
-              {
-                key: "Access-Control-Allow-Methods",
-                value: "GET, POST, PUT, DELETE, OPTIONS",
-              },
-              {
-                key: "Access-Control-Allow-Headers",
-                value: "X-Requested-With, Content-Type, Authorization",
-              },
-            ],
-          },
-        ];
-      },
+  env: {  
+    NEXT_PUBLIC_FIREBASE_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN:  process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    NEXT_PUBLIC_FIREBASE_APP_ID: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+    NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+   }
 }
 
 module.exports = nextConfig
