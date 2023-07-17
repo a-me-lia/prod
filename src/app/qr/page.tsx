4645bf24afd7995710b1259dd2ff1ab25dc6538f
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-07-17 15:02:31 -0400

    qr fix again

--- src/app/qr/page.tsx
+++ src/app/qr/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image"
 
 export default function Page(){
     return(
-        <div className="h-[calc(100dvh)]  xl:min-h-screen">
+        <div className="relative h-[calc(100dvh)]  xl:min-h-screen">
                     <Image src='/qr.png' fill alt='qr code for https://carycspickup.vercel.app' className="aspect-square object-cover"/>
         </div>
 
