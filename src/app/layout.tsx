15fb354d5475642bb6509a2122d4f7b52aa1c13b
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-07-17 00:24:29 -0400

    added metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,9 +19,16 @@ export default function RootLayout({ children }: {
         <head /> will contain the components returned by the nearest parent
         head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
-      <head />
+      <head>
+      <meta property="description" content="The Cary Chinese School summer program pickup handler website" />
+      <meta property="og:image" content="https://carycspickup.vercel.app/_next/image?url=%2Fcss.png&w=3840&q=75">     </meta>
+      <meta property="og:description" content="https://carycspickup.vercel.app" />
+       </head>
       <link rel="icon" href="/favicon.ico" sizes="any" />
       <title>CCS Summer Pickup</title>
+
+      
+ 
       <body className={inter.className}>
         <AuthContextProvider>
           {children}
