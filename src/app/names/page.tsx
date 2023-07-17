a17d058c131e4616af4efe9343d3d277b7910c41
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-07-17 09:36:11 -0400

    names formatting

--- src/app/names/page.tsx
+++ src/app/names/page.tsx
@@ -60,7 +60,7 @@ const Page = () => {
 <div className='flex flex-col items-center  h-[calc(100dvh)]  xl:min-h-screen overflow-hidden bg-slate-700 text-white'>
   <Link className='absolute left-2 top-2 border-2 border-opacity-30 rounded-lg border-white  px-2 py-1'href='/entry'>Back to entry</Link>
 
-  <h1 className='text-2xl mt-16 xl:text-4xl'>Names for pickup:</h1>
+  <h1 className='text-2xl mt-16 mx:mt-2 xl:text-4xl'>Names for pickup:</h1>
   <p className='mx-8 text-center mt-4 mb-2'>click anywhere on the page to enable audio ping!</p>   
   <div className='mt-4 w-full h-1 bg-gradient-to-b from-transparent via-white to-transparent  mx-auto'></div>
   
@@ -70,7 +70,7 @@ const Page = () => {
 
       <ul className="flex flex-col items-center">
         {entries.map((entry, index) => (
-          <li className='flex flex-col text-2xl mt-6 mb-4 xl:w-full xl:mt-12 xl:mb-8 xl:text-7xl font-bold text-center text-white' key={index}>
+          <li className='flex flex-col text-2xl mt-6 mb-4 xl:w-full xl:mt-12 xl:mb-8 xl:text-6xl font-bold text-center text-white' key={index}>
             <span>{entry.username}</span>
             <span className="mt-2 font-normal text-sm xl:mt-4 xl:text-4xl">Timestamp: {entry.timestamp}</span>
             <div className='mt-2 xl:mt-4 w-3/4 h-1 bg-gradient-to-b from-transparent via-white to-transparent  mx-auto'></div>
