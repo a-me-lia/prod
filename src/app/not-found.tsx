a787bc647382a5a3705ffdca4727358807051278
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-07-17 14:10:48 -0400

    404 developed

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -3,21 +3,26 @@ import Image from "next/image";
 
 export default function Custom404() {
   return (
-    <div className=" h-[calc(100dvh)] xl:min-h-screen  overflow-hidden text-black bg-white ">
-      <div className="p-6 flex flex-col space-y-2 items-center">
-        <h1 className="text-6xl">404</h1>
+    <div className=" h-[calc(100dvh)] xl:min-h-screen  overflow-hidden text-black bg-white flex flex-col items-center">
+      <div className="p-6 w-min flex flex-col mt-32 rounded-3xl space-y-2 items-center border-2 border-slate-500">
+        <h1 className="text-8xl font-bold mb-4 border-2 border-red-400 p-4 rounded-xl">404</h1>
         <h1>Page Not Found!</h1>
-        <Link href="/">Back to Sign In</Link>
+        <Link href="/" className="underline">Back to Sign In</Link>
       </div>
+      <div className="absolute bottom-0 w-full">
       <div className="absolute bottom-0 translate-x-1/2">
-        <div className="relative h-72 w-48"></div>
+        <div className="relative h-72 w-48">
         <Image
-          src="/404.png"
+          src="/confuse.png"
           className="object-cover"
           alt="not found!"
           fill
         ></Image>
+        </div>
+
       </div>
+      </div>
+
     </div>
   );
 }
