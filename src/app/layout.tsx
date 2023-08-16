08ed0ccb0fb9e8f3cb1711201432213814016667
matthewguo86 <110259342+a-me-lia@users.noreply.github.com>
2023-08-16 00:26:15 -0400

    default set to english

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   const [carolineIsAllergicToShrimp, setCarolineIsAllergicToShrimp] =
-    useState(false);
+    useState(true);
 
   useEffect(
     () => {
